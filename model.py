880dcb8574d98da489ab3a2158ec8006434e1939
namicindy <132754041+namicindy@users.noreply.github.com>
2026-04-30 10:50:02 +0200

    Update comments for clarity in model.py

--- model.py
+++ model.py
@@ -2,14 +2,14 @@ import torch.nn as nn
 from torchvision import models
 
 def get_model(num_classes=6):
-    # Charger ResNet18 déjà pré-entraîné
+    # Chargement du ResNet18 déjà pré-entraîné
     model = models.resnet18(weights="IMAGENET1K_V1")
 
-    # Geler toutes les couches (on ne retouche pas ce qu'il sait déjà)
+    # Gelation de toutes les couches (sans retoucher ce qu'il sait déjà)
     for param in model.parameters():
         param.requires_grad = False
 
-    # Remplacer la dernière couche pour NOS 6 catégories de déchets
+    # Remplacer la dernière couche par nos 6 catégories de déchets
     model.fc = nn.Linear(512, num_classes)
 
     return model
